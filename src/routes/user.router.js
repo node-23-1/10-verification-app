b8258879cda758ccfccdcd51c8e7c5e9feb2fe0b
Andres Mendoza <andres.david.mm@hotmail.com>
2023-05-19 21:10:06 -0500

    added login

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,18 +1,27 @@
-const { getAll, create, getOne, remove, update, verifyCode } = require('../controllers/user.controllers');
+const { getAll, create, getOne, remove, update, verifyCode, login, getLoggedUser } = require('../controllers/user.controllers');
 const express = require('express');
+const verifyJWT = require('../utils/verifyJWT');
 
 const userRouter = express.Router();
 
 userRouter.route('/')
-    .get(getAll)
+    .get(verifyJWT, getAll)
     .post(create);
 
 userRouter.route('/verify/:code')
     .get(verifyCode);
 
+userRouter.route('/login')
+    .post(login);
+
+userRouter.use(verifyJWT);
+
+userRouter.route('/me')
+    .get(verifyJWT, getLoggedUser);
+
 userRouter.route('/:id')
-    .get(getOne)
-    .delete(remove)
-    .put(update);
+    .get(verifyJWT, getOne)
+    .delete(verifyJWT, remove)
+    .put(verifyJWT, update);
 
 module.exports = userRouter;
